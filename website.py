2eb812383314ebe04399566d3523a56335000078
Matthew Cengia <mattcen@mattcen.com>
2023-09-16 23:19:27 +1000

    Add page to display submitted program ideas
    The final thing we want is to be able to see the program ideas that
    users have submitted.
    
    We'll create a new route which reads from our CSV file and compiles a
    list of authors and ideas. This route will then send that list to a
    newly created `program.html` template to be displayed.
    
    Now we've got this page, we'll update our links so that this page shows
    up after people submit a new program idea, and we'll add a link to the
    page from our Patrol page.

--- website.py
+++ website.py
@@ -23,6 +23,20 @@ def hello(name=None):
     return render_template("hello.html", name=name)
 
 
+@app.get("/program")
+def list_programs():
+    programs = []
+    try:
+        with open(database) as csvfile:
+            csvreader = csv.DictReader(csvfile, fieldnames=fieldnames)
+            for row in csvreader:
+                programs.append(row)
+    except FileNotFoundError:
+        pass
+
+    return render_template("program.html", programs=programs)
+
+
 @app.get("/program/new")
 def new_program():
     return render_template("new_program.html")
@@ -35,7 +49,7 @@ def new_program_post():
         csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
         csvwriter.writerow(program)
 
-    return redirect(url_for("new_program"))
+    return redirect(url_for("list_programs"))
 
 
 if __name__ == "__main__":
