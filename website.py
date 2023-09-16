c98bc9bac8c431c761cb31f38456db9a0dffe006
Matthew Cengia <mattcen@mattcen.com>
2023-09-16 23:43:37 +1000

    Move my-page.html to templates and create a route
    Let's also move our my-page.html to the templates folder, and create a
    route for it.

--- website.py
+++ website.py
@@ -9,5 +9,10 @@ def home():
     return render_template("index.html")
 
 
+@app.get("/my-page")
+def my_page():
+    return render_template("my-page.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=80, host="0.0.0.0")
